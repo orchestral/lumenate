64730fbb5da2e127a44ce20e8a8ee96f72e6ebcd
crynobone <crynobone@gmail.com>
2015-10-06 15:10:07 +0800

    Add bin and basic installer.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/InstallCommand.php
@@ -0,0 +1,54 @@
+<?php namespace Orchestra\LumenateInstaller;
+
+use Symfony\Component\Process\Process;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class InstallCommand extends Command
+{
+    /**
+     * Configure the command options.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this->ignoreValidationErrors();
+
+        $this->setName('install')
+                ->setDescription('Install Lumen into the current project.');
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $composer = $this->findComposer();
+
+        $process = new Process($composer . ' require orchestra/lumen', null, null, null, null);
+
+        $process->run(function ($type, $line) use ($output) {
+            $output->write($line);
+        });
+    }
+
+    /**
+     * Get the composer command for the environment.
+     *
+     * @return string
+     */
+    private function findComposer()
+    {
+        if (file_exists(getcwd() . '/composer.phar')) {
+            return '"' . PHP_BINARY . '" composer.phar"';
+        }
+
+        return 'composer';
+    }
+}
