68a3d3a5799942f506d9e0176807a95ab94c0664
Nasrul Hazim <nasrulhazim.m@gmail.com>
2019-11-14 08:28:19 +0800

    Fixed missing class namespace

--- src/InstallCommand.php
+++ src/InstallCommand.php
@@ -4,6 +4,7 @@ namespace Orchestra\Lumenate;
 
 use Symfony\Component\Process\Process;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
